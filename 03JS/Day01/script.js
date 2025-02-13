93aeb4a3f726c8f8b4c3d56a317c77f000852537
Suraj Kumar Sharma <surajsharma87899@example.com>
2025-02-14 02:01:58 +0530

    added commit

---
+++ 03JS/Day01/script.js
@@ -0,0 +1,23 @@
+console.log("hello coder army")
+// control+`=open terminal
+
+// let const var
+// var old
+// let
+let num=10;
+console.log(num);
+let ar="Rohit";
+ar="Mohit";
+console.log(ar);
+
+// const
+const ids=20;
+console.log(ids);
+// ids=30; values can't be changed
+
+
+// var
+// var old tarika, don't use it.
+var x=10;
+x=20;
+console.log(x);
---
+++ 03JS/Day02/script.js
@@ -0,0 +1,50 @@
+//declare the variable
+let num =10;
+console.log(num);
+
+// primitive Data Type
+// Number,boolean,String, null, undefined, Symbol. bigint
+
+
+let account_balance =20;
+console.log(typeof account_balance);
+
+// string
+
+let str="rohit";
+console.log(typeof str);
+
+let string="rohit negi is a bad boy, he doesn't know how to use zoom";
+console.log(string);
+
+let comment ='hello dosoton';
+console.log(comment);
+
+// boolean
+// true or false
+
+let statement=true;
+console.log(typeof statement);
+
+let Papa_ko_block_kara_hai=false;
+console.log(typeof Papa_ko_block_kara_hai);
+
+
+// undefined
+let account;
+console.log(typeof account);
+
+// Null
+
+let bal=null;
+console.log(bal);
+
+
+//bigint
+
+let a=3445344334345566666666444n;
+console.log(a);
+
+
+// Non Primitive Data Type
+// Array,object,function
