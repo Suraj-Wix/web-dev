b21d7e2e7b17d8155c758c79e27e4657b39384e0
Suraj Kumar Sharma <surajsharma87899@example.com>
2025-03-03 22:20:19 +0530

    added commit

---
+++ 03JS/Day19/j.js
@@ -0,0 +1,13 @@
+// console.log("Hello Coders kaise ho");
+// document.getElementById('first').innerHTML="Hum achee hai";
+
+
+// class Person{
+//   constructor{
+
+//    } 
+// }
+
+// const obj = document.getElementById("first");
+// obj.className="Mohan";
+
